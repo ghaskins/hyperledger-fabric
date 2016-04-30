64fa615f82a53c505508d0ed9f5818a57342c04b
Gregory Haskins <gregory.haskins@gmail.com>
2016-04-29 21:34:09 -0400

    Refactor system-chaincode to use chaintool-based interface
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- core/system_chaincode/sample_syscc/sample_syscc.go
+++ core/system_chaincode/sample_syscc/sample_syscc.go
@@ -21,8 +21,11 @@ package sample_syscc
 
 import (
 	"errors"
+	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/core/system_chaincode/api"
+	"github.com/hyperledger/fabric/core/system_chaincode/sample_syscc/build/ccs"
+	"github.com/hyperledger/fabric/core/system_chaincode/sample_syscc/build/cci/appinit"
+	"github.com/hyperledger/fabric/core/system_chaincode/sample_syscc/build/cci/org/hyperledger/chaincode/sample_syscc"
 )
 
 // SampleSysCC example simple Chaincode implementation
@@ -30,80 +33,56 @@ type SampleSysCC struct {
 }
 
 func init() {
-	api.RegisterSysCC("github.com/hyperledger/fabric/core/system_chaincode/sample_syscc", &SampleSysCC{})
+	self := &SampleSysCC{}
+	interfaces := ccs.Interfaces {
+		"org.hyperledger.chaincode.sample_syscc": self,
+		"appinit": self,
+	}
+
+	err := ccs.Start(interfaces) // Our one instance implements both Transactions and Queries interfaces
+	if err != nil {
+		fmt.Printf("Error starting example chaincode: %s", err)
+	}
 }
 
-func (t *SampleSysCC) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	var key, val string    // Entities
-
-	if len(args) != 2 {
-		return nil, errors.New("need 2 args (key and a value).")
-	}
+func (t *SampleSysCC) Init(stub *shim.ChaincodeStub, params *appinit.Init) error {
 
 	// Initialize the chaincode
-	key = args[0]
-	val = args[1]
-	// Write the state to the ledger
-	err := stub.PutState(key, []byte(val))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
+	return stub.PutState(params.Key, []byte(params.Value))
 }
 
 // Transaction makes payment of X units from A to B
-func (t *SampleSysCC) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	var key, val string    // Entities
+func (t *SampleSysCC) PutVal(stub *shim.ChaincodeStub, params *sample_syscc.KeyValue) error {
 
-	if len(args) != 2 {
-		return nil, errors.New("need 2 args (key and a value).")
-	}
-
-	// Initialize the chaincode
-	key = args[0]
-	val = args[1]
-
-	_, err := stub.GetState(key)
+	_, err := stub.GetState(params.Key)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get val for " + key + "\"}"
-		return nil, errors.New(jsonResp)
+		jsonResp := "{\"Error\":\"Failed to get val for " + params.Key + "\"}"
+		return errors.New(jsonResp)
 	}
 
 	// Write the state to the ledger
-	err = stub.PutState(key, []byte(val))
+	err = stub.PutState(params.Key, []byte(params.Value))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return nil, nil
+	return nil
 }
 
 // Query callback representing the query of a chaincode
-func (t *SampleSysCC) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	if function != "getval" {
-		return nil, errors.New("Invalid query function name. Expecting \"getval\"")
-	}
-	var key string // Entities
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting key to query")
-	}
-
-	key = args[0]
+func (t *SampleSysCC) GetVal(stub *shim.ChaincodeStub, params *sample_syscc.Item) (*sample_syscc.Item, error) {
 
 	// Get the state from the ledger
-	valbytes, err := stub.GetState(key)
+	valbytes, err := stub.GetState(params.Data)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + key + "\"}"
+		jsonResp := "{\"Error\":\"Failed to get state for " + params.Data + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
 	if valbytes == nil {
-		jsonResp := "{\"Error\":\"Nil val for " + key + "\"}"
+		jsonResp := "{\"Error\":\"Nil val for " + params.Data + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	return valbytes, nil
+	return &sample_syscc.Item{Data: string(valbytes)}, nil
 }
