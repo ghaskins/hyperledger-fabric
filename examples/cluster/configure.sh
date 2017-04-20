a627eb585b4eee316f40a3b04b6b892216e4d8f9
Gregory Haskins <gregory.haskins@gmail.com>
2017-04-19 22:30:13 -0400

    Checkpoint
    Change-Id: I76a51d860c5a64d1a018686b840f7dfc6aadd581
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- examples/cluster/configure.sh
+++ examples/cluster/configure.sh
@@ -10,11 +10,9 @@ dockerip () {
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1 | head -n1
 }
 
-orderer=$(dockerip orderer0)
-peer=$(dockerip peer1)
+orderer=$(dockerip orderer)
 
 echo "orderer0:" $orderer
-echo "peer1:" $peer
 
 peer channel create -o $orderer:7050 -c $CHANNEL_NAME -f $CHANNEL_SPEC
 peer channel join -b $CHANNEL_NAME.block
